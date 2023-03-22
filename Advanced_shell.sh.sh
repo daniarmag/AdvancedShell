fb2dd7857ea5a08bdf8f711ab41f71f031b9a58f
daniarmag <75178381+daniarmag@users.noreply.github.com>
2023-03-22 22:07:36 +0200

    Add files via upload

---
+++ Advanced_shell.sh.sh
@@ -0,0 +1,17 @@
+cc Advanced_shell.c -o Advanced_shell
+
+cc CreateFlyer.c -o CreateFlyer
+
+cc GetFlyer.c -o GetFlyer
+
+cc MakeOrder.c -o MakeOrder
+
+cc GetNumComp.c -o GetNumComp
+
+cc GetOrderNum.c -o GetOrderNum
+
+cc GetSummary.c -o GetSummary
+
+cc exit.c -o exit
+
+./Advanced_shell
